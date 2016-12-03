aaab9f260d18fb35e93798ff0d69305b068f059b
Nayed Saïd Ali <nayed.saidali@gmail.com>
2016-12-03 09:46:41 +0100

    roman numerals

---
+++ ruby/roman-numerals/roman_numerals.rb
@@ -0,0 +1,31 @@
+class Fixnum
+  ROMAN_NUMBERS = {
+    "M"   =>  1000,
+    "CM"  =>  900,
+    "D"   =>  500,
+    "CD"  =>  400,
+    "C"   =>  100,
+    "XC"  =>  90,
+    "L"   =>  50,
+    "XL"  =>  40,
+    "X"   =>  10,
+    "IX"  =>  9,
+    "V"   =>  5,
+    "IV"  =>  4,
+    "I"   =>  1
+  }
+  
+  def to_roman
+    roman = ''
+    number = self
+    ROMAN_NUMBERS.each do |rom, arab|
+      roman << rom * (number / arab)
+      number = number % arab
+    end
+    roman
+  end
+end
+
+module BookKeeping
+  VERSION = 2
+end
