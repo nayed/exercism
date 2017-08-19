626dc8b69b94fcc9dfd2206ca67f86e3f3fcd203
Nayed Saïd Ali <nayed.saidali@gmail.com>
2017-08-20 00:54:12 +0200

    testing travis elixir

---
+++ elixir/travis_test.exs
@@ -0,0 +1,6 @@
+test_files = Path.wildcard("*/*/*test.exs")
+Enum.map test_files, fn(file) ->
+  {result, status} = System.cmd("elixir", [file])
+  IO.puts result
+  if status > 0, do: System.halt(1)
+end
