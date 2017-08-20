be4ad7105bf4837051551e01295f060fd540ec5e
Nayed Saïd Ali <nayed.saidali@gmail.com>
2017-08-20 16:13:43 +0200

    better outputing travis tests

--- travis_test.rb
+++ travis_test.rb
@@ -1,21 +1,32 @@
-def run_tests(language, compiler, ending_file_pattern)
+def run_tests(language, compiler, ending_file)
   underline = `tput smul`
   bold = `tput bold`
   red = `tput setaf 1`
   green = `tput setaf 2`
   reset = `tput sgr0`
 
-  test_files = Dir.glob("#{language}/*/#{ending_file_pattern}")
+  test_files = Dir.glob("#{language}/*/*#{ending_file}")
   test_files.map do |file|
+    name = project_name file, ending_file
     system "#{compiler} #{file}"
     if $?.exitstatus > 0
-      system "printf '\n#{bold}#{red}#{underline}#{language.capitalize} test failed.\n'"
+      system "printf '\n#{bold}#{red}#{underline}#{name} test failed.\n'"
       return exit(1) # Exits with "failure" code
     end
 
-    system "printf '\n#{bold}#{green}#{underline}#{language.capitalize} test finished without errors.#{reset}\n\n'"
+    system "printf '\n#{bold}#{green}#{underline}#{name} test finished without error.#{reset}\n\n'"
   end
 end
 
-run_tests "ruby", "ruby", "*test.rb"
-run_tests "elixir", "elixir", "*test.exs"
+def project_name(file, ending_file)
+  file
+    .split("/")
+    .last
+    .chomp(ending_file)
+    .scan(/([A-Za-z0-9]+)/)
+    .map {|word| word.join.capitalize }
+    .join(" ")
+end
+
+run_tests "ruby", "ruby", "test.rb"
+run_tests "elixir", "elixir", "test.exs"
