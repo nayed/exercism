4f15fb5694d9999b9a607e0a1550fa834b0274dd
Nayed Saïd Ali <nayed.saidali@gmail.com>
2017-08-24 01:33:26 +0200

    leap

---
+++ ecmascript/leap/leap.js
@@ -0,0 +1,13 @@
+class Year {
+  constructor(year) {
+    this.year = year
+  }
+
+  isLeap() {
+    return (
+      this.year % 400 === 0 || (this.year % 4 === 0 && this.year % 100 !== 0)
+    )
+  }
+}
+
+export default Year
