c14da6df4241dfabbd0e109b29a7ad88b72db870
Nayed Saïd Ali <nayed.saidali@gmail.com>
2019-03-23 12:18:27 +0100

    run length encoding

---
+++ javascript/run-length-encoding/run-length-encoding.js
@@ -0,0 +1,15 @@
+const encode = plainText => {
+  const consecutiveChars = /([\w\s])\1*/g
+  return plainText.replace(consecutiveChars, match =>
+    match.length > 1 ? match.length + match[0] : match[0]
+  )
+}
+
+const decode = encodedText => {
+  const countAndChar = /(\d+)(\w|\s)/g
+  return encodedText.replace(countAndChar, (match, repeats, char) =>
+    new Array(+repeats + 1).join(char)
+  )
+}
+
+export { encode, decode }
